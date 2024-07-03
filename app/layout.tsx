e61eb4339ea3eaef43b47697aab90bb3e894c8bc
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-07-03 13:57:31 +0530

    dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import { Providers } from "@/components/provider";
+import ThemeSwitch from "@/components/ThemeSwitch";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +18,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ThemeSwitch from "./ThemeSwitch";
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <ThemeSwitch />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
